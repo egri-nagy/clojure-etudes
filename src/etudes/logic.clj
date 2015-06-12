706e3ad5459d6c41498a8d244a7d6643bfba6408
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-12 10:55:27 +1000

    constructing permutation by core.logic

--- src/etudes/logic.clj
+++ src/etudes/logic.clj
@@ -3,6 +3,19 @@
   (:require [clojure.core.logic :as l]
             [clojure.core.logic.fd :as fd]))
 
-(l/run* [q]
-  (l/fresh [x y]
-    (l/== q [x y])))
+;;it is easy to do permutations of fixed size
+(defn permutations3points []
+  (l/run* [q]
+    (l/fresh [x y z]
+      (fd/in x y z (fd/interval 1 3))
+      (fd/distinct [x y z])
+      (l/== q [x y z]))))
+
+;;let's do it for arbitrary number of points -- even easier! :)
+(defn permutations [n]
+  (let [p (vec (repeatedly n l/lvar))
+        points  (fd/interval 1 n)]
+    (l/run* [q]
+      (l/everyg #(fd/in % points) p)
+      (fd/distinct p)
+      (l/== q p))))
