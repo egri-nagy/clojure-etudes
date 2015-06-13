96889fb67148648ae95e983d15f24432d3859222
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-06-13 20:42:11 +1000

    naming lambda function for improving readability

--- src/etudes/combinatorics.clj
+++ src/etudes/combinatorics.clj
@@ -3,10 +3,9 @@
 ;; recursive version
 (defn permutations
   [coll]
-  (if (= 1 (count coll))
-    [coll]
-    (mapcat
-     (fn [x] (map
-              #(cons x %)
-              (permutations (remove #(= x %) coll))))
-     coll)))
+  (letfn [(fix-one-permute-rest [x]
+             (map #(cons x %)
+                  (permutations (remove #(= x %) coll))))]
+    (if (= 1 (count coll))
+      [coll]
+      (mapcat fix-one-permute-rest coll))))
