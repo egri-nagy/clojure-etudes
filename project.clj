73922cf292b1cdbef18ff58fbb77d12aeb2f2b4c
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-07-01 10:21:18 +1000

    clojure 1.7.0 released!

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject etudes "0.1"
   :description "coding exercises in Clojure"
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.logic "0.8.10"]]
   :main ^:skip-aot etudes.core
   :target-path "target/%s"
