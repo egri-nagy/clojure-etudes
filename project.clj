8cb48c210aaef58aad0fe8660a49a68cdf4b2554
Dersu Uzala <attila@egri-nagy.hu>
2017-04-05 12:10:54 +0900

    updating project.clj with plugins

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject etudes "0.1"
   :description "coding exercises in Clojure"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.logic "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.logic "0.8.11"]]
+  :plugins [[lein-kibit "0.1.2"]
+            [lein-ancient "0.6.10"]
+            [lein-bikeshed "0.3.0"]
+            [jonase/eastwood "0.2.3"]]
   :main ^:skip-aot etudes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
