c2e3c5fd11d8bbee0cc5c6e0611ed50ea0f64b53
Dersu Uzala <attila@egri-nagy.hu>
2017-04-08 22:53:21 +0900

    following kibit's recommendations

--- src/etudes/expt.clj
+++ src/etudes/expt.clj
@@ -4,8 +4,8 @@
 ;; the number of ones in the binary representation of n (from SICP)
 ;; using *' to support arbitrary precision
 (defn fast-expt [b n]
-  (cond (= n 0) 1
+  (cond (zero? n) 1
         (even? n) (#(*' % %) (fast-expt b (/ n 2)))
-        :else (*' b (fast-expt b (- n 1)))))
+        :else (*' b (fast-expt b (dec n)))))
 
 (fast-expt 2 32)
