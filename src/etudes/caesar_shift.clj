b18eecd92b763a75fbae27ee63018c11d839a0ab
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-04-19 19:08:39 +0900

    adding docstrings

--- src/etudes/caesar_shift.clj
+++ src/etudes/caesar_shift.clj
@@ -1,20 +1,29 @@
-(ns etudes.caesar-shift)
+(ns etudes.caesar-shift
+  "Caesar shift and its brute-force attack.")
 
 (def letters "abcdefghijklmnopqrstuvwxyz ")
 
 (defn shift-map
+  "A hash-map that maps letters to letters cyclically shifted by n."
   [n]
   (zipmap letters
           (take (count letters)
                 (drop n (cycle letters)))))
 
-(defn encrypter [n]
-  (fn [text] (apply str (map (shift-map n) text))))
+(defn encrypter
+  "A function the encrypts plaintext by an n-shift cipher."
+  [n]
+  (fn [text]
+    (apply str
+           (map (shift-map n) text))))
 
-(defn decrypter [n]
+(defn decrypter
+  "A decrypter is just an encrypter that shifts back."
+  [n]
   (encrypter (mod (- n) (count letters))))
 
 (defn brute-force-attack
+  "Calculates all shifts and thus breaking the cipher."
   [ciphertext]
   (map
    (fn [n] ((decrypter n) ciphertext))
