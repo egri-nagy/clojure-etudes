ff6941f782ac96b0f203fa53faa3b2d0b55f6239
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-04-22 19:10:18 +0900

    element-at

--- src/etudes/list_functions.clj
+++ src/etudes/list_functions.clj
@@ -4,3 +4,10 @@
   "Decides whether a sequence is palindrome or not."
   [l]
   (= l (reverse l)))
+
+(defn element-at
+  "reimplementing nth"
+  [l n]
+  (if (zero? n)
+    (first l)
+    (element-at (rest l) (dec n))))
