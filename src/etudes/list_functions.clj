7f5aad12fb272e7b335a3cccc467c51946766b75
Dersu Uzala <attila@egri-nagy.hu>
2017-05-06 15:50:50 +0900

    another palindrome?

--- src/etudes/list_functions.clj
+++ src/etudes/list_functions.clj
@@ -5,6 +5,17 @@
   [l]
   (= l (reverse l)))
 
+(defn palindrome?2
+  "Decides whether a sequence is palindrome or not,
+  without using reverse."
+  [l]
+  (let [v (vec l)
+        n (count v)]
+    (every? (fn [x] (= (v x)
+                       (v (dec (- n x)))))
+            (range (int (/ n 2))))))
+
+
 (defn element-at
   "reimplementing nth"
   [l n]
