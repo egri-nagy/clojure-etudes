6d06060cc434ced935d0ef3b2794152aec9333bd
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-06-15 12:08:13 +0900

    reducers benchmarks

--- src/etudes/reducers.clj
+++ src/etudes/reducers.clj
@@ -2,12 +2,23 @@
   (:require [clojure.core.reducers :as r]
             [criterium.core :as c]))
 
-(def v (into [] (range 200000)))
+(def v (into [] (range 9000)))
 
-(c/quick-bench (reduce + (map inc (filter even? v))))
+(defn factors [n]
+  (into []
+        (reduce concat
+                (for [x (range 1 (inc (Math/sqrt n))) :when (zero? (rem n x))]
+                  [x (/ n x)]))))
 
-(c/quick-bench (reduce + (r/map inc (r/filter even? v))))
 
-(c/quick-bench (r/fold + (r/map inc (r/filter even? v))))
+(c/bench (reduce + (map count (map factors (filter even? v)))))
 
+(println)
 
+(c/bench (r/reduce + (r/map count (r/map factors (r/filter even? v)))))
+
+(println)
+
+(c/bench (r/fold + (r/map count (r/map factors (r/filter even? v)))))
+
+(println)
