9cae1cd32618a9e368d3aa1f1e3317b4b926e2fe
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-06-20 20:33:10 +0900

    all n-tuples

--- src/etudes/combinatorics.clj
+++ src/etudes/combinatorics.clj
@@ -9,3 +9,35 @@
               (map (partial cons x)
                    (permutations (remove (partial = x) coll))))
             coll)))
+
+(defn tuples1
+  "Wasteful."
+  [n coll]
+  (if (= n 0)
+    [()]
+    (mapcat (fn [x] (map (partial cons x)
+                         (tuples1 (dec n) coll)))
+         coll)))
+
+(defn tuples
+  "All n-tuples formed by the elements of a collection."
+  [n coll]
+  (if (= n 0)
+    [()]
+    (let [n-1-tuples (tuples (dec n) coll)]
+      (mapcat (fn [x] (map (partial cons x)
+                           n-1-tuples))
+              coll))))
+
+;;tuples2
+(defn put-in-front [coll x]
+  (map (partial cons x) coll))
+
+(defn tuples2
+  [n coll]
+  (if (= n 0)
+    [()]
+    (mapcat
+     (partial put-in-front (tuples2 (dec n) coll))
+     coll)))
+
