d5d66e06b518f16566d245289d378f82b76460c6
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-06-24 23:38:36 +0900

    adding repl to latex converter

---
+++ src/etudes/latex_converter.clj
@@ -0,0 +1,25 @@
+(ns etudes.latex-converter
+  (:require [clojure.string :as string]))
+
+(def prompt "etudes.core>")
+
+(defn f [s]
+  (if (string/starts-with? s prompt)
+    (string/replace s prompt "")
+    (str "\\end{CODE}\n\\solution{" s "}\n\\begin{CODE}\n")))
+
+(defn totex [s]
+  (let [s2 (->> (string/split s #"\n")
+                     (map string/trim)
+                     (remove #(= (first %) \#))
+                     (map f)
+                     (remove empty?)
+                     (map string/trim)
+                     (map #(str % \newline))
+                     (apply str))
+        s3 (->> (string/split s2 #"\n")
+                (map #(str % \newline)))]
+    (apply str (cons "\\begin{CODE}\n" (take (dec (count s3)) s3)))))
+
+(println (totex (slurp "resources/x")))
+(println)
