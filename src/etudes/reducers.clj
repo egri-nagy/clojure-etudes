01efb5436da75c315fb19476b8ac6b66db759f35
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-07-06 10:57:47 +0900

    testing tesser as well

--- src/etudes/reducers.clj
+++ src/etudes/reducers.clj
@@ -3,9 +3,11 @@
             [tesser.core :as t]
             [criterium.core :as c]))
 
+(def CORES (.. Runtime getRuntime availableProcessors))
+
 (def v (into [] (repeatedly 10000  #(rand-int 1000000000))))
 
-(defn factors [n]
+(defn divisors [n]
   (into []
         (reduce concat
                 (for [x (range 1 (inc (Math/sqrt n)))
@@ -21,40 +23,33 @@
 ;; (println)
 ;; (println "futures and derefs")
 
-;; (c/bench (let [f1 (future (reduce MAX (map count (map factors (filter even? (range 200000 201000))))))
-;;                f2 (future (reduce MAX (map count (map factors (filter even? (range 201000 202000))))))
-;;                f3 (future (reduce MAX (map count (map factors (filter even? (range 202000 203000))))))
-;;                f4 (future (reduce MAX (map count (map factors (filter even? (range 203000 204000))))))]
+;; (c/bench (let [f1 (future (reduce MAX (map count (map divisors (filter even? (range 200000 201000))))))
+;;                f2 (future (reduce MAX (map count (map divisors (filter even? (range 201000 202000))))))
+;;                f3 (future (reduce MAX (map count (map divisors (filter even? (range 202000 203000))))))
+;;                f4 (future (reduce MAX (map count (map divisors (filter even? (range 203000 204000))))))]
 ;;            (reduce MAX 0 [@f1 @f2 @f3 @f4])))
 
-;; (println)
-;; (println "tesser")
 
-;; ;(c/bench (t/tesser (t/chunk 1024 v) (t/reduce MAX (t/map count (t/map factors (t/filter even? v))))))
+;; ;(c/bench (t/tesser (t/chunk 1024 v) (t/reduce MAX (t/map count (t/map divisors (t/filter even? v))))))
 
-;; (c/bench
-;;  (->> (t/filter even?)
-;;       (t/map factors)
-;;       (t/map count)
-;;       (t/fold MAX)
-;;       (t/tesser (t/chunk 1024 v))))
+ (println)
+ (println "tesser")
+ (c/bench
+  (->> (t/filter even?)
+       (t/map divisors)
+       (t/map count)
+       (t/fold MAX)
+       (t/tesser (t/chunk 512 v))))
 
-
-;; (println)
-;; (println "vanilla")
-
-;; (c/bench (reduce MAX (map count (map factors (filter even? v)))))
-
+(println)
+(println "vanilla")
+(c/bench (reduce MAX (map count (map divisors (filter even? v)))))
 
 (println)
 (println "reducers")
-
-(c/bench (r/reduce MAX (r/map count (r/map factors (r/filter even? v)))))
-
+(c/bench (r/reduce MAX (r/map count (r/map divisors (r/filter even? v)))))
 
 (println)
 (println "folders")
-
-(c/bench (r/fold MAX MAX (r/map count (r/map factors (r/filter even? v)))))
-
+(c/bench (r/fold MAX MAX (r/map count (r/map divisors (r/filter even? v)))))
 (println)
