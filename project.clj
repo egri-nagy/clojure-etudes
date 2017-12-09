13b2a742979057357f84b2e996d13e9d4d18b632
Attila Egri-Nagy <attila@egri-nagy.hu>
2017-12-09 13:11:32 +0900

    clj, lein-ancient update

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject etudes "0.1"
   :description "coding exercises in Clojure"
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.logic "0.8.11"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [tesser.core "1.0.2"]
                  [criterium "0.4.4"]]
   :plugins [[lein-kibit "0.1.2"]
-            [lein-ancient "0.6.10"]
+            [lein-ancient "0.6.14"]
             [lein-bikeshed "0.3.0"]
             [jonase/eastwood "0.2.3"]]
   :main ^:skip-aot etudes.core
