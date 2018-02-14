ccf4ceaffc9ae09631b570da41a0dac246a00c13
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-02-14 22:25:21 +0900

    prepping towers of hanoi

--- src/etudes/hanoi.clj
+++ src/etudes/hanoi.clj
@@ -1,12 +1,11 @@
 (ns etudes.hanoi)
 
 (defn towers-of-hanoi
-  ([numofdisks] (towers-of-hanoi numofdisks :rodA :rodB :rodC))
-  ([numofdisks from to via]
-   (if (= 1 numofdisks) (println (format "%s to %s" from to))
-       (do
-         (towers-of-hanoi (dec numofdisks) from via to)
-         (println (format "%s to %s" from to))
-         (recur (dec numofdisks) via to from)))))
+  [numofdisks source target spare]
+   (if (= 1 numofdisks) (str " " source "->" target " ")
+       (str
+         (towers-of-hanoi (dec numofdisks) source spare target)
+         (str " " source "->" target " ")
+         (towers-of-hanoi (dec numofdisks) spare target source))))
 
-(towers-of-hanoi 3)
+(towers-of-hanoi 2 :t1 :t2 :t3)
