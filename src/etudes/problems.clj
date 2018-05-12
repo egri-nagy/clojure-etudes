c97af043b63172e72b826a15e5b6967cf96f4d40
Dersu Uzala <attila@egri-nagy.hu>
2018-05-12 16:17:20 +0900

    2-digit version is fine

--- src/etudes/problems.clj
+++ src/etudes/problems.clj
@@ -58,11 +58,19 @@
   [s]
   (apply str (filter (set "0123456789.") s)))
 
-(defn posint->english
+(defn posint2digit->english
   [n]
-  (let [digits {0 "zero" 1 "one" 2 "two" 3 "three" 4 "four" 5 "five" 6 "six" 7 "seven"
-                8 "eight" 9 "nine" }
-        teens {}]))
+  (let [smallnums {0 "zero" 1 "one" 2 "two" 3 "three" 4 "four" 5 "five" 6 "six"
+                   7 "seven" 8 "eight" 9 "nine"
+                   ;; numbers between 10 and 20 are still irregular enough
+                   10 "ten" 11 "eleven" 12 "twelve" 13 "thirteen" 14 "fourteen"
+                   15 "fifteen" 16 "sixteen" 17 "seventeen" 18 "eighteen"
+                   19 "nineteen"}
+        tens {2 "twenty" 3 "thirty" 4 "forty" 5 "fifty" 6 "sixty" 7 "seventy"
+              8 "eighty" 9 "ninety"}]
+    (if (< n 20)
+      (smallnums n)
+      (str (tens (int (/ n 10))) "-" (smallnums (mod n 10))) )))
 
 (defn dollars
   [s])
