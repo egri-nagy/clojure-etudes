fd6d09b3c35a4a3d06cf0d5e7c3bf60849a4837d
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-12-02 13:45:01 +0900

    adding ij correction

--- src/etudes/sgf.clj
+++ src/etudes/sgf.clj
@@ -2,13 +2,16 @@
   "Functions for working with the Smart Game Format"
   (:require [clojure.string :as string]))
 
-  (def letters "abcdefghijklmnopqrstuvwxyz")
+(def letters "abcdefghijklmnopqrstuvwxyz")
+(def nojletters "abcdefghiklmnopqrstuvwxyz")
 
 (def letter->coord (zipmap letters (range 1 (inc (count letters)))))
+
+(def letter->nojletter (zipmap letters nojletters))
 
 (defn sgfcoords->igocoords
   [s]
   (let [coords (re-seq #"[a-z]+" s)
-        converted (map (fn [cc] (str (first cc)
+        converted (map (fn [cc] (str (letter->nojletter (first cc))
                                      (letter->coord (second cc)))) coords)]
     (string/join "," converted)))
