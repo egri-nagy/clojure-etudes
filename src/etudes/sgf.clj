e951a532e0047a9b2a76d99ecb82997825191a01
Attila Egri-Nagy <attila@egri-nagy.hu>
2018-12-02 13:55:09 +0900

    it's the other letter

--- src/etudes/sgf.clj
+++ src/etudes/sgf.clj
@@ -3,15 +3,15 @@
   (:require [clojure.string :as string]))
 
 (def letters "abcdefghijklmnopqrstuvwxyz")
-(def nojletters "abcdefghiklmnopqrstuvwxyz")
+  (def noiletters "abcdefghjklmnopqrstuvwxyz")
 
 (def letter->coord (zipmap letters (range 1 (inc (count letters)))))
 
-(def letter->nojletter (zipmap letters nojletters))
+(def letter->noiletter (zipmap letters noiletters))
 
 (defn sgfcoords->igocoords
   [s]
   (let [coords (re-seq #"[a-z]+" s)
-        converted (map (fn [cc] (str (letter->nojletter (first cc))
+        converted (map (fn [cc] (str (letter->noiletter (first cc))
                                      (letter->coord (second cc)))) coords)]
     (string/join "," converted)))
