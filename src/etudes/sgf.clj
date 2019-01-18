68c9f1192e25d854082e39aa6234be4e52e28fee
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-01-18 09:12:59 +0900

    separate the individual coord conversion

--- src/etudes/sgf.clj
+++ src/etudes/sgf.clj
@@ -3,15 +3,20 @@
   (:require [clojure.string :as string]))
 
 (def letters "abcdefghijklmnopqrstuvwxyz")
-  (def noiletters "abcdefghjklmnopqrstuvwxyz")
+(def noiletters "abcdefghjklmnopqrstuvwxyz")
 
-(def letter->coord (zipmap letters (range 1 (inc (count letters)))))
+(def letter->coord (zipmap letters
+                           (range 1 (inc (count letters)))))
 
 (def letter->noiletter (zipmap letters noiletters))
+
+(defn sgfcoord->igocoord
+  [cc]
+  (str (letter->noiletter (first cc))
+       (letter->coord (second cc))))
 
 (defn sgfcoords->igocoords
   [s]
   (let [coords (re-seq #"[a-z]+" s)
-        converted (map (fn [cc] (str (letter->noiletter (first cc))
-                                     (letter->coord (second cc)))) coords)]
+        converted (map sgfcoord->igocoord coords)]
     (string/join "," converted)))
