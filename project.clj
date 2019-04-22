5c84d3b633a75a08d0575fcb3ddded35c1ff637d
Attila Egri-Nagy <attila@egri-nagy.hu>
2019-04-22 21:05:15 +0900

    updating dep

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/core.match "0.3.0"]
                  [quil "3.0.0"]
                  [tesser.core "1.0.3"]
-                 [criterium "0.4.4"]
+                 [criterium "0.4.5"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [nrepl "0.6.0"]]
   :plugins [[lein-kibit "0.1.2"]
