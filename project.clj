9056e05ac7caebde3c3c518dc726bfe193fab485
Attila Egri-Nagy <egri-nagy@Attilas-Mac-mini.local>
2022-07-28 08:50:28 +0900

    updating deps

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject etudes "0.1"
   :description "coding exercises in Clojure"
-  :dependencies [[org.clojure/clojure "1.11.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.logic "1.0.1"]
                  [org.clojure/core.match "1.0.0"]
                  [quil "3.1.0"]
-                 [tesser.core "1.0.4"]
+                 [tesser.core "1.0.5"]
                  [criterium "0.4.6"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [nrepl "0.9.0"]]
   :plugins [[lein-kibit "0.1.8"]
             [lein-ancient "0.7.0"]
             [lein-bikeshed "0.5.2"]
-            [jonase/eastwood "1.2.3"]]
+            [jonase/eastwood "1.2.4"]]
   :main ^:skip-aot etudes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
