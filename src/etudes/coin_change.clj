6b3c9af3a9f58396ca66f7947cfe473bb47c6517
egri-nagy <attila@egri-nagy.hu>
2024-12-20 08:52:25 +0900

    getting all the coin changes

--- src/etudes/coin_change.clj
+++ src/etudes/coin_change.clj
@@ -1,10 +1,25 @@
 (ns etudes.coin-change)
 
-(defn coin-change [coins sum]
+(defn count-coin-changes [coins sum]
   (cond
     (zero? sum) 1
     (or (neg? sum) (empty? coins)) 0
-    :else (+ (coin-change coins (- sum (first coins)))
-             (coin-change (rest coins) sum))))
+    :else (+ (count-coin-changes coins (- sum (first coins)))
+             (count-coin-changes (rest coins) sum))))
 
-(coin-change [1 3] 7)
+(count-coin-changes [1 3] 7)
+
+(defn coin-changes
+  ;; adding the collection for the chosen coins
+  ([coins sum] (coin-changes coins sum []))
+  ([coins sum changes]
+   (cond
+     (zero? sum) [changes] ;we made it, we have the sum!
+     (or (neg? sum) (empty? coins)) [] ;not enough coins or too big sum
+     :else (reduce into []
+                   [(let [coin (first coins)]
+                      (coin-changes coins (- sum coin)
+                                    (conj changes coin)))
+                    (coin-changes (rest coins) sum changes)]))))
+
+(coin-changes [1 2 3 5] 10)
